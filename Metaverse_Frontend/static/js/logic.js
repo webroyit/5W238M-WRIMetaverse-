7f4fed0b9a98cbc4eb177d4c75540b3763b42685
royweb <xytank@gmail.com>
2022-05-31 13:48:56 -0400

    Draw canvas of Map Metaverse

--- Metaverse_Frontend/static/js/logic.js
+++ Metaverse_Frontend/static/js/logic.js
@@ -1,1 +1,24 @@
 //Logic.js file
+
+// Graphics constants
+// - Map Management Constants
+const tiles = 12;                       // In pixels
+const plots = tiles * 9;                // In pixels
+const roads = tiles * 2;                // In pixels
+
+//- Canvas and Context
+const mainCanvas = document.getElementById("mainCanvas");
+const mainCtx = mainCanvas.getContext('2d');
+const worldImage = new Image();
+
+// Canvas drawing functions
+function drawCanvas(){
+    mainCanvas.width = 3 * plots + 4 * roads;
+    mainCanvas.height = 3 * plots + 4 * roads;
+    worldImage.src = 'static/img/Moraland.png';
+    worldImage.onload = () => {
+        mainCtx.drawImage(worldImage, 0, 0);
+    }
+}
+
+drawCanvas();
