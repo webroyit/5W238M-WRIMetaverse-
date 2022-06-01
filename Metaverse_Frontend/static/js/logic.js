1862698f0918575fb070ec030f326d55047edcd6
royweb <xytank@gmail.com>
2022-06-01 15:00:42 -0400

    Update code

--- Metaverse_Frontend/static/js/logic.js
+++ Metaverse_Frontend/static/js/logic.js
@@ -5,11 +5,16 @@
 const tiles = 12;                       // In pixels
 const plots = tiles * 9;                // In pixels
 const roads = tiles * 2;                // In pixels
+const initialOffsets = plots + roads;
+const plotViewOffsets = plots + (2 * roads);
 
 //- Canvas and Context
 const mainCanvas = document.getElementById("mainCanvas");
 const mainCtx = mainCanvas.getContext('2d');
 const worldImage = new Image();
+
+//- State
+const mapView = {mapOffsetX:-1 * initialOffsets, mapOffsetY:-1 * initialOffsets};
 
 // Canvas drawing functions
 function drawCanvas(){
@@ -17,9 +22,22 @@ function drawCanvas(){
     mainCanvas.height = 3 * plots + 4 * roads;
     worldImage.src = 'static/img/Moraland.png';
     worldImage.onload = () => {
-        mainCtx.drawImage(worldImage, -1 * (plots + roads), -1 * (plots + roads));
-        mainCtx.strokeRect(plots + (2 * roads), plots + (2 * roads), plots, plots);     // Add border for selected area
+        initializeMap();
     }
+}
+
+function initializeMap() {
+    drawMapSection(mainCtx, mapView.mapOffsetX, mapView.mapOffsetY);
+    drawCursor(plotViewOffsets, plotViewOffsets);
+}
+
+function drawMapSection(ctx,originX,originY){
+    ctx.drawImage(worldImage,originX,originY);
+}
+
+// Add border for selected area
+function drawCursor(x,y) {
+    mainCtx.strokeRect(x,y,plots,plots);
 }
 
 drawCanvas();
