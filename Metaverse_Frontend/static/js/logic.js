01fbd850f6f0b9922cc680a27fb98820d78589b9
royweb <xytank@gmail.com>
2022-06-02 11:54:08 -0400

    Display Current Plot

--- Metaverse_Frontend/static/js/logic.js
+++ Metaverse_Frontend/static/js/logic.js
@@ -11,15 +11,20 @@ const plotViewOffsets = plots + (2 * roads);
 //- Canvas and Context
 const mainCanvas = document.getElementById("mainCanvas");
 const mainCtx = mainCanvas.getContext('2d');
+const plotCanvas = document.getElementById("plotCanvas");
+const plotCtx = plotCanvas.getContext('2d');
 const worldImage = new Image();
 
 //- State
 const mapView = {mapOffsetX:-1 * initialOffsets, mapOffsetY:-1 * initialOffsets};
+const plotView = {plotX:0, plotY:0, locationX:0, locationY:0};
 
 // Canvas drawing functions
 function drawCanvas(){
     mainCanvas.width = 3 * plots + 4 * roads;
     mainCanvas.height = 3 * plots + 4 * roads;
+    plotCanvas.width = plots;
+    plotCanvas.height = plots;
     worldImage.src = 'static/img/Moraland.png';
     worldImage.onload = () => {
         initializeMap();
@@ -27,8 +32,10 @@ function drawCanvas(){
 }
 
 function initializeMap() {
+    updatePlotLocation();
     drawMapSection(mainCtx, mapView.mapOffsetX, mapView.mapOffsetY);
     drawCursor(plotViewOffsets, plotViewOffsets);
+    drawMapSection(plotCtx, -1 * plotView.locationX, -1 * plotView.locationY);
 }
 
 function drawMapSection(ctx,originX,originY){
@@ -38,6 +45,11 @@ function drawMapSection(ctx,originX,originY){
 // Add border for selected area
 function drawCursor(x,y) {
     mainCtx.strokeRect(x,y,plots,plots);
+}
+
+function updatePlotLocation() {
+    plotView.locationX = -1 * mapView.mapOffsetX + plotViewOffsets;
+    plotView.locationY = -1 * mapView.mapOffsetY + plotViewOffsets;
 }
 
 drawCanvas();
